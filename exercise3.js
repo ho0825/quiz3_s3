8c7e31397d0a94e29d5f036c0379c9c21aa9dd25
ho <hozheheng@gmail.com>
2023-04-06 15:40:33 +0800

    change 2

--- exercise3.js
+++ exercise3.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+
+    res.send('Log in')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello UTeM!')
+})
+
+app.post('/log', (req, res) => {
+    res.send('Hi ho')
+  })
+
+app.get('/register', (req, res) => {
+    res.send('Hi khoo, tan')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
