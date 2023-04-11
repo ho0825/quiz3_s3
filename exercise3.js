96f2ccf79ec3a658f2b9e6889aa9b3a8f2f12097
ho <hozheheng@gmail.com>
2023-04-11 14:44:27 +0800

    change 3

--- exercise3.js
+++ exercise3.js
@@ -2,12 +2,64 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let dbmongoUsers = [
+    {
+        username : "ho",
+        password : 1234,
+        name : "HO",
+        email : "ho@utem.student.edu.my"
+    },
+    {
+        username : "khoo",
+        password : 12345,
+        name : "KHOO",
+        email : "khoo@utem.student.edu.my"
+    }
+]
+
+function login(reqUsername, reqPassword){
+    let matchuser = dbmongoUsers.find(user => user.username == reqUsername)
+    if (!matchuser) return "User not found!"
+
+    
+    if (matchuser.password == reqPassword){
+        return matchuser
+    }
+    else {
+        return "Wrong password!"
+    }
+
+}
+
+function signup(reqUsername, reqPassword, reqName, reqEmail){
+    dbmongoUsers.push({
+        username : reqUsername,
+        password : reqPassword,
+        name : reqName,
+        email : reqEmail,
+    })
+    return "Registered Successful"
+}
+
 app.use(express.json())
+
+app.post('/signup', (req, res) => {
+    let result = signup (
+        req.body.Username,
+        req.body.Password,
+        req.body.Name,
+        req.body.Email
+    )
+
+    res.send(result)
+})
 
 app.post('/login', (req, res) => {
     console.log(req.body)
 
-    res.send('Log in')
+    let result = login(req.body.Username,req.body.Password)
+
+    res.send(result)
 })
 
 app.get('/', (req, res) => {
